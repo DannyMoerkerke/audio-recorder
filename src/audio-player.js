61451b333a5bcf89b8120ae3299aed0760bdbaef
Danny Moerkerke <danny@dannymoerkerke.nl>
2019-02-05 23:24:14 +0100

    - added equalizer

--- src/audio-player.js
+++ src/audio-player.js
@@ -1,4 +1,5 @@
 import '../node_modules/material-custom-elements/src/material-button.js';
+import '../node_modules/material-custom-elements/src/material-slider.js';
 
 export class AudioPlayer extends HTMLElement {
 
@@ -17,6 +18,12 @@ export class AudioPlayer extends HTMLElement {
           :host {
             display: block;
             border: 1px solid #ff0000;
+          }
+          
+          material-slider {
+            --track-height: 1px;
+            --thumb-size: 12px;
+            
           }
           
           #container {
@@ -39,6 +46,17 @@ export class AudioPlayer extends HTMLElement {
             width: 0;
             border-right: 1px solid #0000ff;
           }
+          #frequencies-container {
+            position: absolute;
+            top: 0;
+            left: 0;
+            opacity: 0;
+          }
+          
+          #equalizer-container {
+            width: 200px;
+            transform: rotate(-90deg);
+          }
       </style>
       
       <input type="file" id="file-input">
@@ -46,7 +64,10 @@ export class AudioPlayer extends HTMLElement {
         <i class="material-icons" slot="left-icon">play_arrow</i>
       </material-button>
       
-      <div id="time"></div>
+      <material-slider id="volume" value="100" max="100"></material-slider>
+      
+      <span id="elapsed-time"></span> / <span id="total-time"></span>
+      
       <div id="container">
         <div id="waveform-container">
           <canvas id="waveform" width="400" height="400"></canvas>
@@ -54,6 +75,10 @@ export class AudioPlayer extends HTMLElement {
         
         <div id="progress-container">
           <canvas id="progress" width="400" height="400"></canvas>
+        </div>
+        
+        <div id="frequencies-container">
+          <canvas id="frequencies" width="400" height="400"></canvas>
         </div>
         
         <div id="processing-progress"></div>
@@ -68,7 +93,9 @@ export class AudioPlayer extends HTMLElement {
     this.playing = false;
     this.ready = false;
     this.context = new AudioContext();
-    this.source = this.context.createBufferSource();
+    this.gainNode = this.context.createGain();
+    this.output = this.context.destination;
+    this.analyser = this.context.createAnalyser();
     this.downloadProgress = 0;
     this.downloadProgressComputable = false;
     this.processingProgress = 0;
@@ -79,26 +106,89 @@ export class AudioPlayer extends HTMLElement {
     this.pauseTime = 0;
     this.audioBuffers = [];
 
-    this.timeDisplay = {
-      hours: '00',
-      seconds: '00',
-      minutes: '00'
-    };
-    this.maxChunkLength = 1024000 * 50;
+    this.maxChunkLength = 524000; //1024000 * 50;
     this.canvas = this.shadowRoot.querySelector('#waveform');
     this.canvasContext = this.canvas.getContext('2d');
     this.progressCanvas = this.shadowRoot.querySelector('#progress');
     this.progressCanvasContext = this.progressCanvas.getContext('2d');
     this.container = this.shadowRoot.querySelector('#container');
+    this.frequencyCanvas = this.shadowRoot.querySelector('#frequencies');
+    this.frequencyCanvasContext = this.frequencyCanvas.getContext('2d');
     this.waveformContainer = this.shadowRoot.querySelector('#waveform-container');
     this.progressContainer = this.shadowRoot.querySelector('#progress-container');
+    this.frequenciesContainer = this.shadowRoot.querySelector('#frequencies-container');
 
     this.processingProgressBar = this.shadowRoot.querySelector('#processing-progress');
     this.processingPercentage = this.shadowRoot.querySelector('#processing-percentage');
     this.fileInput = this.shadowRoot.querySelector('#file-input');
     this.playButton = this.shadowRoot.querySelector('#play');
     this.playButtonIcon = this.playButton.querySelector('i');
-    this.time = this.shadowRoot.querySelector('#time');
+    this.elapsedTime = this.shadowRoot.querySelector('#elapsed-time');
+    this.totalTime = this.shadowRoot.querySelector('#total-time');
+    this.volume = this.shadowRoot.querySelector('#volume');
+    this.audio = this.shadowRoot.querySelector('audio');
+
+    const filters = [
+      {
+        type: 'lowshelf',
+        frequency: 64.0,
+      },
+      {
+        type: 'peaking',
+        frequency: 125.0,
+        q: 0.5
+      },
+      {
+        type: 'peaking',
+        frequency: 500.0,
+        q: 0.5
+      },
+      {
+        type: 'peaking',
+        frequency: 1000.0,
+        q: 0.5
+      },
+      {
+        type: 'peaking',
+        frequency: 2000.0,
+        q: 0.5
+      },
+      {
+        type: 'peaking',
+        frequency: 4000.0,
+        q: 0.5
+      },
+      {
+        type: 'peaking',
+        frequency: 8000.0,
+        q: 0.5
+      },
+      {
+        type: 'highshelf',
+        frequency: 16000.0
+      }
+    ];
+
+    this.filters = filters.map(({type, frequency, q}) => {
+      const filter = this.context.createBiquadFilter();
+      filter.type = type;
+      filter.frequency.value = frequency;
+
+      if(q) {
+        filter.Q.value = q;
+      }
+
+      filter.gain.value = 0.0;
+
+      return filter;
+    });
+
+    this.equalizer = this.filters.reduce((destination, filter) => {
+      destination.connect(filter);
+      return filter;
+    }, this.gainNode);
+
+    this.createEqualizerHTML(this.filters);
   }
 
   connectedCallback() {
@@ -120,9 +210,40 @@ export class AudioPlayer extends HTMLElement {
     this.canvasWidth = this.canvas.width;
     this.canvasHeight = this.canvas.height;
 
+    this.showTotalTime(0);
+    this.showElapsedTime(0);
+
     this.container.addEventListener('click', this.handleWaveformClick.bind(this));
     this.fileInput.addEventListener('change', this.loadFile.bind(this));
     this.playButton.addEventListener('click', this.playPause.bind(this));
+    this.volume.addEventListener('change', e => this.setVolume(e.detail.value / 100));
+  }
+
+  createEqualizerHTML(filters) {
+    const fragment = document.createDocumentFragment();
+    const div = document.createElement('div');
+    div.id = 'equalizer-container';
+    const eqContainer = fragment.appendChild(div);
+
+    filters.forEach((filter, i) => {
+      const html = `<material-slider min="-10" max="10" value="0" data-filter="${i}"></material-slider>`;
+      eqContainer.insertAdjacentHTML('beforeend', html);
+      const slider = eqContainer.lastChild;
+
+      slider.addEventListener('change', (e) => {
+        console.log(slider.dataset.filter);
+        const gain = e.detail.value;
+        const index = slider.dataset.filter;
+        filters[index].gain.setValueAtTime(gain, this.context.currentTime);
+      });
+
+    });
+
+    this.container.before(fragment);
+  }
+
+  setVolume(value) {
+    this.gainNode.gain.setValueAtTime(value, this.context.currentTime);
   }
 
   handleWaveformClick(e) {
@@ -166,10 +287,10 @@ export class AudioPlayer extends HTMLElement {
     const audioBuffers = await this.getAudioBuffers(buffer);
     this.audioBuffers = audioBuffers;
     this.duration = audioBuffers.reduce((total, buffer) => total + buffer.duration, 0);
-
+    console.log('audioBuffers', audioBuffers.length);
     const waveformData = this.getWaveformData(audioBuffers);
     this.renderWaveform(waveformData);
-    this.showElapsedTime(0);
+    this.showTotalTime(this.duration);
   }
 
   sliceAudio(buffer, start, end) {
@@ -237,7 +358,7 @@ export class AudioPlayer extends HTMLElement {
     return audioBuffers.map(buffer => {
       const source = this.context.createBufferSource();
       source.buffer = buffer;
-      source.connect(this.context.destination);
+      // source.connect(this.context.destination);
 
       return source;
     });
@@ -272,7 +393,9 @@ export class AudioPlayer extends HTMLElement {
         };
 
         this.curSource = src;
-        src.start(0, offset);
+        this.curSource.connect(this.gainNode);
+        this.equalizer.connect(this.output);
+        this.curSource.start(0, offset);
       }
       else {
         this.stopAudio();
@@ -282,6 +405,7 @@ export class AudioPlayer extends HTMLElement {
     const [nodes, offset = this.pauseTime] = this.pauseTime > 0 ? this.getNodesAfterOffset(audioNodes, this.pauseTime) : [audioNodes];
 
     playSources(nodes, offset);
+    // this.freq();
   }
 
   playPause() {
@@ -316,33 +440,76 @@ export class AudioPlayer extends HTMLElement {
     cancelAnimationFrame(this.timerId);
 
     this.pauseTime = 0;
-    this.timeDisplay = {
-      hours: '00',
-      seconds: '00',
-      minutes: '00'
-    };
-
     this.progressContainer.style.width = 0;
     this.showElapsedTime(0);
   }
 
-  showElapsedTime(secs) {
+  formatTime(secs) {
     const minute = 60;
     const hour = 3600;
 
-    this.hours = Math.floor(secs / hour);
-    this.minutes = Math.floor((secs % hour) / minute);
-    this.seconds = Math.floor(secs) % minute;
+    const hrs = Math.floor(secs / hour);
+    const min = Math.floor((secs % hour) / minute);
+    const sec = Math.floor(secs) % minute;
 
-    // store seconds in this.seconds to only update the display once per second
-    if(secs !== this.secs) {
-      this.secs = secs;
-      this.timeDisplay.hours = this.hours < 10 ? `0${this.hours}` : this.hours;
-      this.timeDisplay.seconds = this.seconds < 10 ? `0${this.seconds}` : this.seconds;
-      this.timeDisplay.minutes = this.minutes < 10 ? `0${this.minutes}` : this.minutes;
-    }
-    const {hours, minutes, seconds} = this.timeDisplay;
-    this.time.innerHTML = `${hours}:${minutes}:${seconds}`;
+    const hours = hrs < 10 ? `0${hrs}` : hrs;
+    const minutes = min < 10 ? `0${min}` : min;
+    const seconds = sec < 10 ? `0${sec}` : sec;
+    return hours !== '00' ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`;
+  }
+
+  showElapsedTime(secs) {
+    this.elapsedTime.innerHTML = this.formatTime(secs);
+  }
+
+  showTotalTime(secs) {
+    this.totalTime.innerHTML = this.formatTime(secs);
+  }
+
+  freq() {
+    this.analyser.fftSize = 256;
+    const bufferLength = this.analyser.frequencyBinCount;
+
+    const dataArray = new Float32Array(bufferLength);
+    console.log(dataArray);
+    this.frequencyCanvasContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+    const draw = () => {
+      // requestAnimationFrame(draw);
+      console.log(dataArray);
+      this.analyser.getFloatFrequencyData(dataArray);
+      console.log(dataArray);
+      this.frequencyCanvasContext.fillStyle = 'rgb(255, 255, 255)';
+      this.frequencyCanvasContext.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+      const barWidth = (this.canvasWidth / bufferLength) * 2.5;
+      let x = 0;
+
+      // dataArray.map((_, i) => {
+      //   const barHeight = (dataArray[i] + 140) * 2;
+      //
+      //   this.frequencyCanvasContext.fillStyle = `rgb(${Math.floor(barHeight + 100)},50,50)`;
+      //   this.frequencyCanvasContext.fillRect(x, this.canvasHeight - barHeight / 2, barWidth, barHeight / 2);
+      //
+      //   x += barWidth + 1;
+      // });
+
+      // console.log(bufferLength);
+
+      for(var i = 0; i < 10; i++) {
+        let barHeight = (dataArray[i] + 140)*2;
+
+        this.frequencyCanvasContext.fillStyle = '#ff0000'; //'rgb(' + Math.floor(barHeight+100) + ',50,50)';
+        this.frequencyCanvasContext.fillRect(x,this.canvasHeight-barHeight/2,barWidth,barHeight/2);
+
+        // console.log(x, this.canvasHeight - barHeight / 2, barWidth, barHeight / 2);
+        // console.log(dataArray[i], barHeight);
+
+        x += barWidth + 1;
+      }
+    };
+
+    draw();
   }
 
   getWaveformData(buffers) {
