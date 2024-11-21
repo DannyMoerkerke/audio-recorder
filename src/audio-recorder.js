fbf098d5974cd8c6a129b66daa5e00b3d0c94c6e
Danny Moerkerke <danny@dannymoerkerke.nl>
2024-11-21 20:14:01 +0100

    updated to new command and commandfor syntax

--- src/audio-recorder.js
+++ src/audio-recorder.js
@@ -9,7 +9,8 @@ export class AudioRecorder extends HTMLElement {
 
     const shadowRoot = this.attachShadow({mode: 'open'});
 
-    shadowRoot.innerHTML = `
+
+    shadowRoot.innerHTML = /*css*/`
       <style>
         :host {
           --width: 210px;
@@ -263,27 +264,27 @@ export class AudioRecorder extends HTMLElement {
         
         <div id="controls">
           <div id="buttons">
-            <button id="capture-audio" part="button" invoketarget="container" invokeaction="capture">
+            <button id="capture-audio" part="button" commandfor="container" command="--capture">
               <i class="material-icons">mic</i>
             </button>
             
-            <button id="stop-capture-audio" part="button" invoketarget="container" invokeaction="stop-capture">
+            <button id="stop-capture-audio" part="button" commandfor="container" command="--stop-capture">
               <i class="material-icons">mic_off</i>
             </button>
             
-            <button id="play" part="button" invoketarget="container" invokeaction="play">
+            <button id="play" part="button" commandfor="container" command="--play">
               <i class="material-icons">play_arrow</i>
             </button>
             
-            <button id="pause" part="button" invoketarget="container" invokeaction="pause">
+            <button id="pause" part="button" commandfor="container" command="--pause">
               <i class="material-icons">pause</i>
             </button>
       
-            <button id="record-audio" part="button" invoketarget="container" invokeaction="record">
+            <button id="record-audio" part="button" commandfor="container" command="--record">
               <i class="material-icons">fiber_manual_record</i>
             </button>
             
-            <button id="stop-record-audio" part="button" invoketarget="container" invokeaction="stop-record">
+            <button id="stop-record-audio" part="button" commandfor="container" command="--stop-record">
               <i class="material-icons">stop</i>
             </button>
             
@@ -293,23 +294,23 @@ export class AudioRecorder extends HTMLElement {
               </a>
             </button>
             
-            <button id="frequencies-button" part="button" invoketarget="container" invokeaction="show-frequencies">
+            <button id="frequencies-button" part="button" commandfor="container" command="--show-frequencies">
               <i class="material-icons">equalizer</i>
             </button>
             
-            <button id="waveform-button" part="button" invoketarget="container" invokeaction="show-waveform">
+            <button id="waveform-button" part="button" commandfor="container" command="--show-waveform">
               <i class="material-icons">graphic_eq</i>
             </button>
           </div>
           
           <div id="volume-container">
-            <button id="volume-min" circle part="volume-button" invoketarget="container" invokeaction="volume-min">
+            <button id="volume-min" circle part="volume-button" commandfor="container" command="--volume-min">
               <i class="material-icons">volume_off</i>
             </button>
             
             <input type="range" id="volume" value="1" min="0" max="1" step="0.01" part="slider" invoketarget="container" invokeaction="volume">
             
-            <button id="volume-max" circle part="volume-button" invoketarget="container" invokeaction="volume-max">
+            <button id="volume-max" circle part="volume-button" commandfor="container" command="--volume-max">
               <i class="material-icons">volume_up</i>
             </button>
           </div>
@@ -346,22 +347,22 @@ export class AudioRecorder extends HTMLElement {
     this.frequencyCanvasContext = this.frequencyCanvas.getContext('2d');
     this.progressContainer = this.shadowRoot.querySelector('#progress-container');
 
-    this.addEventListener('invoke', ({action}) => {
-      const actions = {
-        'capture': this.captureAudio,
-        'stop-capture': this.stopCaptureAudio,
-        'play': this.playPause,
-        'pause': this.playPause,
-        'record': this.recordAudio,
-        'stop-record': this.stopRecordAudio,
-        'show-frequencies': this.showFrequencyAnalyzer,
-        'show-waveform': this.showWaveform,
-        'volume-min': () => this.setVolume(0),
-        'volume-max': () => this.setVolume(1),
+    this.addEventListener('command', ({command}) => {
+      const commands = {
+        '--capture': this.captureAudio,
+        '--stop-capture': this.stopCaptureAudio,
+        '--play': this.playPause,
+        '--pause': this.playPause,
+        '--record': this.recordAudio,
+        '--stop-record': this.stopRecordAudio,
+        '--show-frequencies': this.showFrequencyAnalyzer,
+        '--show-waveform': this.showWaveform,
+        '--volume-min': () => this.setVolume(0),
+        '--volume-max': () => this.setVolume(1),
       }
 
-      if(actions[action]) {
-        actions[action].call(this);
+      if(commands[command]) {
+        commands[command].call(this);
       }
     });
 
